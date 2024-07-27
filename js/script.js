fbfa60f4acf7227ecc03e6be2b39b77c920deab0
Leonardo-Bauza <162306889+Leonardo-Bauza@users.noreply.github.com>
2024-07-26 21:56:07 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,54 +1,50 @@
-let pokemonList = []; // Inicializar pokemonList como un array vacío
+// Inicializar pokemonList como un array vacío
+let pokemonList = [];
 
 // Cargar los datos de Pokémon
 fetch("./db/data.JSON")
   .then(response => response.json())
-  .then(data => {
+  .then(data => {;
     pokemonList = data;
     renderList(pokemonList);
   })
   .catch(error => console.error('Error fetching Pokémon data:', error));
 
 // Función para guardar los datos del usuario en localStorage
-const saveUserData = () => {
+  const saveUserData = () => {;
   const userData = {};
-
-  pokemonList.forEach(pokemon => {
+  pokemonList.forEach(pokemon => {;
     const maleCheckbox = document.getElementById(`${pokemon.id}-male`);
     const femaleCheckbox = document.getElementById(`${pokemon.id}-female`);
-    
     if (maleCheckbox) userData[`${pokemon.id}-male`] = maleCheckbox.checked;
     if (femaleCheckbox) userData[`${pokemon.id}-female`] = femaleCheckbox.checked;
   });
-
   localStorage.setItem('userData', JSON.stringify(userData));
 };
 
 // Inicializar el estado de los checkboxes desde localStorage
-const initializeCheckboxes = () => {
+const initializeCheckboxes = () => {;
   const userData = JSON.parse(localStorage.getItem('userData')) || {};
-  
-  pokemonList.forEach(pokemon => {
+    pokemonList.forEach(pokemon => {;
     const maleCheckbox = document.getElementById(`${pokemon.id}-male`);
     const femaleCheckbox = document.getElementById(`${pokemon.id}-female`);
     
-    if (maleCheckbox) {
+    if (maleCheckbox) {;
       maleCheckbox.checked = userData[`${pokemon.id}-male`] || false;
       maleCheckbox.addEventListener('change', saveUserData);
     }
-    if (femaleCheckbox) {
+    if (femaleCheckbox) {;
       femaleCheckbox.checked = userData[`${pokemon.id}-female`] || false;
       femaleCheckbox.addEventListener('change', saveUserData);
-    }
+    };
   });
 };
 
 // Función para renderizar la lista de Pokémon
-function renderList(pokemonArray) {
+function renderList(pokemonArray) {;
   const pokemonContainer = document.getElementById("pokemon-container");
   pokemonContainer.innerHTML = ''; // Limpiar el contenedor
-  
-  pokemonArray.forEach(pokemon => {
+  pokemonArray.forEach(pokemon => {;
     const pokemonCard = document.createElement("div");
     pokemonCard.className = 'pokemon-card';
     pokemonCard.innerHTML = `
@@ -63,39 +59,34 @@ function renderList(pokemonArray) {
         </label>
         </label>
       <button class="selectPokemon" id="${pokemon.id}">Agregar</button>`;
-    
     pokemonContainer.appendChild(pokemonCard);
   });
 
   addMissing(); // Llamar a la función addMissing después de renderizar
   initializeCheckboxes(); // Inicializar los checkboxes después de renderizar
-}
+};
 
 // Función para manejar la adición de Pokémon al carrito
-function addMissing() {
+function addMissing() {;
   let showMissingPokemon = JSON.parse(localStorage.getItem('showMissingPokemon')) || [];
-  
   const addButton = document.querySelectorAll(".selectPokemon");
-  addButton.forEach(button => {
-    button.onclick = (e) => {
+  addButton.forEach(button => {;
+    button.onclick = (e) => {;
       const pokemonId = e.currentTarget.id;
       const selectedPokemon = pokemonList.find(pokemon => pokemon.id == pokemonId);
-      if (selectedPokemon && !showMissingPokemon.some(pokemon => pokemon.id === selectedPokemon.id)) {
+      if (selectedPokemon && !showMissingPokemon.some(pokemon => pokemon.id === selectedPokemon.id)) {;
         showMissingPokemon.push(selectedPokemon);
         showMissingPokemon.sort((a, b) => a.id - b.id);
         localStorage.setItem("showMissingPokemon", JSON.stringify(showMissingPokemon));
-      }
+      };
     };
   });
-}
+};
 
 // Función para eliminar todos los datos de localStorage
-const clearLocalStorage = () => {
+const clearLocalStorage = () => {;
     localStorage.clear();
-    // Opcional: Redibujar la lista de Pokémon si es necesario
     pokemonList = [];
     renderList(pokemonList);
   };
-  
-  // Configurar el evento para el botón DeleteAll
   document.getElementById('deleteAll').addEventListener('click', clearLocalStorage);
