a5c5cc03c347099e4acc5114b4055f38eb031a5f
Leonardo-Bauza <162306889+Leonardo-Bauza@users.noreply.github.com>
2024-07-27 14:50:49 -0300

    Add files via upload

--- missingList/missings.js
+++ missingList/missings.js
@@ -1,11 +1,11 @@
-let cartStorage = localStorage.getItem("showMissingPokemon");
-cartStorage = JSON.parse(cartStorage) || [];
+let missingStorage = localStorage.getItem("showMissingPokemon");
+missingStorage = JSON.parse(missingStorage) || [];
 
-let cartContainer = document.getElementById("pokemon-container");
+let missingContainer = document.getElementById("pokemon-container");
 
-function renderCarrito(carritoPokemon) {;
-    cartContainer.innerHTML = '';
-    carritoPokemon.forEach(pokemon => {;
+function renderMissingContainer(MissingPokemonArray) {;
+    missingContainer.innerHTML = '';
+    MissingPokemonArray.forEach(pokemon => {;
         const pokemonCard = document.createElement("div");
         pokemonCard.className = 'pokemon-card'
         pokemonCard.innerHTML = `<h3>${pokemon.name}</h3>
@@ -16,32 +16,54 @@ function renderCarrito(carritoPokemon) {;
                                     <label> <input type="checkbox" id="${pokemon.id}-female" ${userData[`${pokemon.id}-female`] ? 'checked' : ''}>♀
                                     </label>
                                 </label>
-                                <button class="eliminateSelected" id="${pokemon.id}">Quitar </button>`
-        cartContainer.appendChild(pokemonCard);
+                                <a class="eliminateSelected" id="${pokemon.id}">Quitar </a>`
+        missingContainer.appendChild(pokemonCard);
     });
-    eraseMissing(cartStorage);
+    eraseMissing(missingStorage);
 };
 const userData = JSON.parse(localStorage.getItem('userData')) || {};
 
 // Funcion para Eliminar del Carrito
 function eraseMissing() {;
-    const addButton = document.querySelectorAll(".eliminateSelected");
-    addButton.forEach(button => (
+    const eraseButton = document.querySelectorAll(".eliminateSelected");
+    eraseButton.forEach(button => (
         button.onclick = (e) => {;
             const pokemonId = e.currentTarget.id;
-            cartStorage = cartStorage.filter(pokemon => pokemon.id.toString() !== pokemonId);
-            localStorage.setItem("showMissingPokemon", JSON.stringify(cartStorage));
-            renderCarrito(cartStorage);
-        } 
+            missingStorage = missingStorage.filter(pokemon => pokemon.id.toString() !== pokemonId);
+            localStorage.setItem("showMissingPokemon", JSON.stringify(missingStorage));
+            renderMissingContainer(missingStorage);
+            Toastifier("Eliminado");
+        }
     ));  
 };
 
-renderCarrito(cartStorage);
+renderMissingContainer(missingStorage);
 
 // Función para eliminar todos los datos de localStorage
 const clearLocalStorage = () => {;
     localStorage.clear();
-    pokemonList = [];
-    renderList(pokemonList);
+    missingStorage = [];
+    renderMissingContainer(missingStorage);
+    Toastifier("Listado vaciado");
   };
   document.getElementById('deleteAll').addEventListener('click', clearLocalStorage);
+
+    /* Librería Toastify */
+  function Toastifier(message) {
+    Toastify({
+    text: message,
+    duration: 4000,
+    destination: "./missings.html",
+    newWindow: false,
+    close: false,
+    gravity: "top", // `top` or `bottom`
+    position: "right", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #101010, #FE2020, #001100)",
+      color: "white",
+    },
+    onClick: function(){
+    } // Callback after click
+    }).showToast();
+  }
